e88601662cd187b89d437c5b2e5aa0ca5b1bce38
yayano <boussetta13yahia@gmail.com>
2023-02-13 18:23:46 +0000

    add footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,18 @@
+import footer from "../../images/footer-bg.svg";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>footer</h1>
+      <div className="dev-name">BOUSSETTA YAHIA</div>
+      <div className="created-with">
+        <span>Copyright</span>
+        <b>©</b>
+        <span>2022-2023</span>
+        <span>Boussetta Yahia</span>
+        <span className="love">
+          Created with
+          <i className="fa fa-heart"></i>
+        </span>
+      </div>
     </div>
   );
 };

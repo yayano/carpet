da0c386d82e738e751f0257b9c228db8c95fb5fd
yayano <boussetta13yahia@gmail.com>
2023-03-19 19:03:48 +0000

    add costumer model

---
+++ backend/models/Costumer.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const constumerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    carpets: {
+      data: Array,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("costumer", constumerSchema);

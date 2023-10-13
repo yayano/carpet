307c4f08895e630179cce3882ffc49fb5671dd4f
yayano <boussetta13yahia@gmail.com>
2023-10-13 23:30:19 +0100

    new version

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,22 @@
-const express = require("express");
-
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
-app.listen(4000, () => {
-  console.log("listening on port 4000");
-});
+/* use middleware look expressjs.com*/
+app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+app.use(cors());
+
+const CONNECTION_URI = process.env.CONNECTION_URI;
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, console.log(`server is Running on port :${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+ 
